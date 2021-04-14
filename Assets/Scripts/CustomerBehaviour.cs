ed6bfa77e01aa330dc21e5445bed982a0f5b13cb
Faraz2010 <faraz0098@Farazs-MBP.hub>
2021-04-14 13:57:36 +1000

    CustomerBehaviour

---
+++ Assets/Scripts/CustomerBehaviour.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CustomerBehaviour : MonoBehaviour
+{
+    [SerializeField]
+    float speed;
+    [SerializeField]
+    float range;
+    [SerializeField]
+    float maxDistance;
+
+    Vector2 wayPoint;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position = Vector2.MoveTowards(transform.position, wayPoint, speed * Time.deltaTime);
+        if (Vector2.Distance(transform.position, wayPoint) < range)
+        {
+            SetNewDestiation();
+        }
+    }
+    void SetNewDestiation()
+    {
+        wayPoint = new Vector2(Random.Range(-maxDistance, maxDistance), Random.Range(-maxDistance, maxDistance));
+    }
+}
